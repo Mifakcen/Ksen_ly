5413416106706ad6585af61f1710a355699df51b
Mif <misha.fedorov.2012@bk.ru>
2019-05-30 00:22:10 +0300

    Финишь уже видно Осталось Реализовать пункты "Г" и "Д"

--- Ksen_Ly/Ksen_Ly.cpp
+++ Ksen_Ly/Ksen_Ly.cpp
@@ -6,74 +6,193 @@
 #include <string>
 #include <list>
 /*
-Реализовать методы обработки текстовых формул. Формулы должны содержать математические операции сумма (вычитание = сумма числа с противоположным знаком),
 
-умножение, деление. В качестве операндов математических формул выступают·
-
-числа формата с плавающей запятой и знаком, причем разделитель дробной части может быть опущен( пример: 1, 1.2, -6.4, -4 );
-
-· индексы процессов в массиве ProcessArray, индексы отделяются от числе специальным символом ( пример: $3, %11, &4 ).
-
-Математические операции могут быть записаны в виде функций, например SUM(-5, $1),
-
-что будет означать добавление -5 к каждому элементу массива ProcessArray[1].Data. 
-
-Разделителем между операндами является символ ','. Пробелы не должны влиять на работоспособность формул.
-
-Помимо формулы необходимо задать индекс процесса в массиве ProcessArray. Если Data==NULL, то необходимо выделить память.
-
-В случае если calculated==true, то данные пересчитываются по новой формуле, в противном случае операция прерывается.
 */
 struct Process
 {
-	float *Data; // данные процесса, в случае отсутствия данных Data==NULL
+	float *Data;      // данные процесса, в случае отсутствия данных Data==NULL
 	bool calculated; // флаг, true в случае если данные вычислены по формуле
 };
-string out();
+struct Command
+{
+	string com;           //Команда которую вводит пользователь
+	float data;         // Данные которое пользователь указывает как первый параметр 
+	int  *addressProc; //Адреса процессов которые хочет изменить пользователь 
+};
+Command *out();
+void inp(Process *);
+int N = 3;                        //Количество доступных процессов
+Process *ProcessArray = new Process[N];
+   // Про исходные данные нам не говорят
+  // По этому будем предпологать, что у нас только три процесса
+ //Также будем считать, что первая цифра данных процеса указывает на количество значений 
+//Из этого следует, что у нас будет на одно значение больше 
+
 int main()
 {
-	setlocale(0, "RUS");
-	const int N = 3;                        //Количество доступных процессов
-	Process *ProcessArray = new Process[N];// Про исходные данные нам не говорят
-	string str;                           // По этому будем предпологать, что у нас только три процесса
-	                                     //Также будем считать, что первая цифра данных процеса указывает на количество значений 
-                                        //Из этого следует, что у нас будет на одно значение больше 
-	ProcessArray[0].Data = new float[6] {5, 1.1111, 11.488, 11, 0, 0.123};
-	ProcessArray[1].Data = new float[6] {5, 56, 2.2222 , 23.1241, 1, 0 };
-	ProcessArray[2].Data = new float[6] {5, -38 ,23,3.3333333333 ,0, 1};
+	string str;
+	Command *command = NULL;        //Наша строка содержит команду, которая делится на три значения :
+	setlocale(0, "RUS");           //Первое это идентификатор функции (SUM)
+								  //Второе это значение (на)которое мы собираемся (делить/умножать)прибавлять 
+								 //Третье это номера процессов к которым мы будем применять действие
+	ProcessArray[0].Data = new float[6]{ 5, 1.1111, 11.488, 11, 0, 0.123 };
+	ProcessArray[0].calculated = true;
+	ProcessArray[1].Data = new float[6]{ 5, 56, 2.2222 , 23.1241, 1, 0 };
+	ProcessArray[1].calculated = false;
+	ProcessArray[2].Data = new float[6]{ 5, -38 ,23,3.3333333333 ,0, 1 };
+	ProcessArray[2].calculated = true;
+
 	do {
-		system("CLS");
-		cout << "Для получения справки введите '-help'  \nВведите команду : \n";	
-		str = out();
-		if (str == "-Sum") {
-
-			break;
+		cout << "\n Для получения справки введите '-help'  \nВведите команду : \n";
+		command = out();
+		cout <<command->addressProc[0];
+		cout <<command->addressProc[1];
+		cout <<command->addressProc[2];
+		cout <<command->addressProc[3];
+		if (command->com == "-INF") {
+			inp(ProcessArray);
 		}
-		if (str == "Div") {
-
-			break;
+		if (command->com == "-clear") {
+			system("CLS");
+			continue;
 		}
-		if (str == "Multi") {
+		if (command->com == "-SUM" || command->com == "-DIV" || command->com == "MULTI") {
+			for (int y = 1; y <= command->addressProc[0]; y++) {
+				if ((command->addressProc[y]) < N) {
+					if (ProcessArray[command->addressProc[y]].calculated) {
+						if (command->com == "-SUM") {
+							cout << "Sum\n";
+							for (int i = 1; i < ProcessArray[(command->addressProc[y])].Data[0]; i++) {
+								ProcessArray[(command->addressProc[y])].Data[i] += (command->data);								
+							}
+							continue;
+						}
+						if (command->com == "-DIV") {
+							if ((command->data) == 0) {
+								cout << "      \n Деление на ноль ! \n";
+								break;
+							}
+							cout << "Div\n";
+							for (int i = 1; i < ProcessArray[(command->addressProc[y])].Data[0]; i++) {
+								ProcessArray[(command->addressProc[y])].Data[i] /= (command->data);								
+							}
+							continue;
+						}
+						if (command->com == "-MULTI") {
+							if ((command->addressProc[y]) < N) {
+								cout << "Multi\n";
+								for (int i = 1; i < ProcessArray[(command->addressProc[y])].Data[0]; i++) {
+									ProcessArray[(command->addressProc[y])].Data[i] *= (command->data);									
+								}
+								continue;
+							}
+						}
+						
+					}
+					else { cout << "\n  Доступ закрыт.\n"; continue; }
+				}
+				else cout << "   Ошибка!   Адрес процесса за пределами массива ! \n";
 
-			break;
+			}inp(ProcessArray);
 		}
-	} while (str != "-exit");
-
+		if (command->com == "-help") {
+			cout << "Help\n";
+			continue;
+		}
+	} while (command->com != "-exit");
 }
 
-string out() {
+Command *out() {
 	string str;
 	string s;
-	getline(cin, s);
-	char flag = ' ';
+	getline(cin, s,';');
+
+	char flagC = ' ';
+
+	int R_L = 0; //счетчик количества скобок (Если отличен от нуля, то скобок не хватает)
+	int Size = 0; //cчетчик количества процессов
+	flagC = ' ';
+
+	int flagI = 1;      //флаг для этапа функции
+	string flagD = ""; //флаг для стрингов
+	int flagSize = 1; //флаг для оперантов
+
 	for (int i = 0; i < s.size(); i++) {
-		flag = s[i];
-		if (flag != ' ')
-			str += flag;
+		flagC = s[i];
+		if (flagC != ' '  && flagC!='\n')
+			str += flagC;
+		if (flagC == '(') {
+			R_L++;
+			continue;
+		}
+		if (flagC == ')') {
+			R_L--;
+			continue;
+		}
+		if (flagC == '$') {
+			Size++;
+			continue;
+		}
 	}
-	return str;
+	Command *comand = new Command;
+
+	for (int i = 0; i < str.size(); i++) {
+		flagC = str[i];
+		if (flagC == '(') {
+			flagI = 2;
+			continue;
+		};
+		//if (flagC == '$') {flagI = 3;}
+		if (flagI == 1) { 
+			comand->com += flagC; 
+			continue;
+		};
+		if (flagI == 2) {
+			if (flagC == ',') {
+				comand->data = stoi(flagD);
+				flagI = 3;
+				flagD = "";
+				comand->addressProc = new int[Size + 1];//Выделили место
+				comand->addressProc[0] = Size;     //определили количество оперантов
+				cout << comand->addressProc[0];
+				flagD = "";
+				continue;
+			}
+			flagD += flagC;
+			//comand->data += flagC;
+			continue;
+		};
+		if (flagI == 3) {
+			if (flagC == ',') {
+				comand->addressProc[flagSize] = stoi(flagD);
+				flagSize++;
+				continue;
+			}
+			
+			if (flagC == '$') {
+				flagD = "";
+				continue;
+			}
+			if (flagC == ')') {
+				comand->addressProc[flagSize] = stoi(flagD);
+				break;
+			}
+			//comand->addressProc += flagC;
+			flagD += flagC;
+			continue;
+		};
+	}
+	return comand;
 }
 
+void inp(Process *ProcessArray) {
+	for (int i = 0; i < N; i++) {
+		cout<<"\n   Process #"<< i<< ":"<<endl;
+		for (int j = 1; j < ProcessArray[i].Data[0]; j++) {
+			cout << "       Data[" << j << "] = " << ProcessArray[i].Data[j];
+		}
+	}
+}
 
 // Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
 // Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
